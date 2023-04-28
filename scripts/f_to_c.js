7708f6a5784492aa300a4edbf1bda013f4e69a9e
Nerio Guio <andrik1379@gmail.com>
2023-04-28 13:54:36 -0400

    converter

---
+++ scripts/f_to_c.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const txtInput = document.getElementById("txtInput");
+const txtOutput = document.getElementById("txtOutput");
+const calculateBtn = document.getElementById("calculateBtn");
+
+window.onload = init;
+
+function init(){
+    calculateBtn.onclick = onCalculateBtnClicked;
+}
+
+function onCalculateBtnClicked(){
+    console.log("Clicked!!");
+
+    let userTypedValue = txtInput.value;
+    let valueAsNumber = parseFloat(userTypedValue);
+    console.log("Got the value from the textbox it was: " + userTypedValue);
+
+    let celsius = (valueAsNumber - 32) * 5/9;
+    console.log("calculated the value of: " + celsius);
+
+    txtOutput.value = celsius;
+
+}
